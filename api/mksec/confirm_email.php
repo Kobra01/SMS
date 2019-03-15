6f52a7dc3a352f7da44b2aeb3eb9c9a03ba5fe14
Maximilian Kromer <a.makro@web.de>
2019-03-15 17:26:49 +0100

    added main code

--- api/mksec/confirm_email.php
+++ api/mksec/confirm_email.php
@@ -9,30 +9,45 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 // files needed to connect to database
 include_once 'config/Database.php';
 include_once 'objects/User.php';
+include_once 'objects/Code.php';
  
 // get database connection
 $database = new Database();
 $db = $database->getConnection();
  
-// instantiate other objects
+// instantiate objects
 $user = new User($db);
+$code = new Code($db);
 
 // set product property values
-$user->code = $_GET['code'];
+$code->type = '1';
+$code->code = isset($_GET['code']) ? $_GET['code'] : die();
 
-// confirm the email
-if (!$user->confirmEmail()) {
+// verify code
+if (!$code->verifyCode()) {
 
     // failed to confirm the email
     http_response_code(400);
-    echo json_encode(array("error" => TRUE, "message" => "Failed to confirm email address."));
+    echo json_encode(array("error" => TRUE, "message" => "Failed to confirm code."));
 
 } else {
 
-    // set response code & answer
-    http_response_code(200);
-    echo json_encode(array("error" => FALSE, "message" => "Email address succesfull confirmed."));
+    $user->id = $code->user_id;
+    $user->state = '1';
 
+    if (!$user->updateState()) {
+        
+        // failed to confirm the email
+        http_response_code(400);
+        echo json_encode(array("error" => TRUE, "message" => "Failed to change user state."));
+
+    } else {
+        
+        // set response code & answer
+        http_response_code(200);
+        echo json_encode(array("error" => FALSE, "message" => "Email address succesfull confirmed."));
+
+    }    
 }
 
 
