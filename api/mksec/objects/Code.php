ae2c1cc3f2132c2b60460cc020cdc2898fe97cfc
Maximilian Kromer <a.makro@web.de>
2019-03-15 17:25:38 +0100

    fixes and include verifyCode option

--- api/mksec/objects/Code.php
+++ api/mksec/objects/Code.php
@@ -11,6 +11,7 @@ class Code {
     public $id;
     public $code;
     public $user_id;
+    public $type;
  
     // constructor
     public function __construct($db){
@@ -18,7 +19,7 @@ class Code {
     }
 
     //create new E-Mail verify code
-    public function createCode($codetype) {
+    public function createCode() {
 
         // Create Query
         $query = 'INSERT INTO ' . $this->table_name . '
@@ -35,11 +36,11 @@ class Code {
 
         $this->code=htmlspecialchars(strip_tags($this->code));
         $this->user_id=htmlspecialchars(strip_tags($this->user_id));
-        $this->type=htmlspecialchars(strip_tags($this->codetype));
+        $this->type=htmlspecialchars(strip_tags($this->type));
 
         $stmt->bindParam(':user', $this->user_id);
         $stmt->bindParam(':code', $this->code);
-        $stmt->bindParam(':type', $this->codetype);
+        $stmt->bindParam(':type', $this->type);
 
         // exit if failed
         if(!$stmt->execute()){
@@ -50,6 +51,57 @@ class Code {
 
     }
 
-    
+    public function verifyCode(){
+
+        // Create Query
+        $query = '  SELECT
+                        id, user
+                    FROM
+                        ' . $this->table_name . '
+                    WHERE
+                        code = :code
+                        AND
+                        type = :type
+                        AND
+                        created > :created
+                    LIMIT
+                        0,1';
+
+
+        // prepare the query
+        $stmt = $this->conn->prepare($query);
+
+        $createTime = strtotime('-24 hours');
+    	$timestamp = date('Y-m-d H:i:s', $createTime);
+
+
+        $this->code=htmlspecialchars(strip_tags($this->code));
+        $this->type=htmlspecialchars(strip_tags($this->type));
+        $this->type=htmlspecialchars(strip_tags($timestamp));
+
+        $stmt->bindParam(':code', $this->code);
+        $stmt->bindParam(':type', $this->type);
+        $stmt->bindParam(':type', $timestamp);
+
+        // exit if failed
+        if(!$stmt->execute()){
+            return false;
+        }
+        
+        if (!$stmt->rowCount() > 0) {
+            return false;
+        }
+
+        // get record details / values
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+ 
+        // assign values to object properties
+        $this->id = $row['id'];
+        $this->user_id = $row['user'];
+
+        return true;
+    }
 
 }
+
+?>
